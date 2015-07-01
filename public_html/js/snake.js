73de35de191cb8bd161469bd6cad256a79c650f1
jcamberos <tucamberos@gmail.com>
2015-07-01 14:57:25 -0700

    Food Position
    Food position was set as within the snake screen. Snake will be able to pass next to or through the food.

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -88,7 +88,7 @@ function snakeUpdate() {
    if(snakeDirection == "down"){
        snakeHeadY++;
    }
-    else{
+    else if (snakeDirection == "right"){
        snakeHeadX++; 
     }
     var snakeTail = snake.pop();
@@ -110,15 +110,16 @@ function foodInitialize() {
 }
 function foodDraw() {
     context.fillStyle = "white";
-    context.fillRect(food.x, food.y, snakeSize, snakeSize);
+    context.fillRect(food.x * snakeSize, food.y * snakeSize, snakeSize, snakeSize);
     
 }
 
 function setFoodPosition() {
     var randomX = Math.floor(Math.random()* screenWidth);
     var randomY = Math.floor(Math.random()* screenHeight);
-    food.x = randomX;
-    food.y = randomY;
+    
+    food.x = Math.floor(randomX / snakeSize);
+    food.y = Math.floor(randomY / snakeSize);
 }
 /*-----------------------------------------------------------
  * Input Functions;
@@ -131,7 +132,9 @@ function keyboardHandler(event){
     if(event.keyCode == "39") {
         snakeDirection = "right";
     }
-    
+    else if(event.keyCode == "40") {
+        snakeDirection = "down";
+    }
 }
 
     
