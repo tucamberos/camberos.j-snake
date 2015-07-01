1f732a851fb2104a76d97be99f3ec4e7505441ff
jcamberos <tucamberos@gmail.com>
2015-07-01 08:59:29 -0700

    Food
    Added movement to the location of the food.

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -98,9 +98,17 @@ function foodInitialize() {
         x: 0,
         y: 0,
     };
+    setFoodPosition();
 }
 function foodDraw() {
     context.fillStyle = "white";
     context.fillRect(food.x, food.y, snakeSize, snakeSize);
     
+}
+
+function setFoodPosition() {
+    var randomX = Math.floor(Math.random()* screenWidth);
+    var randomY = Math.floor(Math.random()* screenHeight);
+    food.x = randomX;
+    food.y = randomY;
 }
