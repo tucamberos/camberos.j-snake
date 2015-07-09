8c6fff9420d29020bda061ad402061750d1e29fe
jcamberos <tucamberos@gmail.com>
2015-07-09 12:18:20 -0700

    center menu
    Fixed errors that were preventing from text being center on the screen

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -214,8 +214,8 @@ function showMenu(state) {
 }
 
 function centerMenuPosition(menu) {
-    menu.style.top = (screenHeight / 2) + "px";
-    
+    menu.style.top = (screenHeight / 2) - (menu.offsetHeight / 2) + "px";
+    menu.style.left = (screenWidth / 2) - (menu.offsetWidth / 2) + "px";
 }
 
 
